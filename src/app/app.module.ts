db9546028505ee17c24a85577ea9d8eb7b02b381
Melinda Marino <melindamarino22@gmail.com>
2020-04-13 14:59:41 -0500

    components made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    CreateTodoComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { ITodo } from '../interfaces/ITodo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  todoList: ITodo[] = [
+    { id: 1, title: 'Install Angular CLI', isDone: false },
+  ];
+  todoId = 0;
+
   constructor() { }
+
+  addTodo(title: string): void {
+    this.todoList.push({
+      id: this.todoId++,
+      title,
+      isDone: false
+    });
+  }
+
+  deleteTodo(todo: ITodo) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
+
+  getTodos(): ITodo[] {
+    return this.todoList;
+  }
 }
